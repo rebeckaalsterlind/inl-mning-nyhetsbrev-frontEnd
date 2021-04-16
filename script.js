6b07e1237ff30d8a2ba9572e2d7411c1e88d0d4d
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-04-16 19:24:19 +0200

    reg new user

--- script.js
+++ script.js
@@ -10,30 +10,60 @@ const header = document.querySelector("header");
 const headerAside = document.querySelector("#headerAside");
 const root = document.querySelector("#root");
 
-//TEMPLATES
+//////TEMPLATES
+//REGISTER
 const regHeadTemp = `
-<div>
+<section>
     <input type="text" id="username" name="username" placeholder="Username" autocomplete="on">
     <input type="password" id="password" name="password" placeholder="Password" autocomplete="on">
-    <input type="submit" value="Log in" id="logInBtn">
-</div>`;
-const regMainTemp = `<div><h1>form to register</h1></div>`;
+    <input type="submit" value="Log in" id="logInBtn" />
+</section>`;
+const regMainTemp = `
+<section">
+    <h2>Create an account</h2>
+    <div>Username <input id="regUN" type="text" name="username"></div>
+    <div>Firstname <input id="firstname" type="text" name="firstname"></div>
+    <div>Lastname <input id="lastname" type="text" name="lastname"></div>
+    <div>Email <input id="email" type="email" name="email"></div>
+    <div>Password <input id="regPW" type="password" name="password"></div>
+    <div>Subscribe to newsletter <input id="checkbox" type="checkbox" name="newsletter"></div>
+    <div><button id="createAccount">Create</button></div>
+</section>`;
 
+//LOGGED OUT
 const loggedOutHeadTemp = `<div><button id="regBtn">Register</button></div>` + regHeadTemp;
 const loggedOutMainTemp = `<div><h1>log in to access your account</h1></div>`;
 
-const loggedInHeadTemp = `<div><button id="logOutBtn">Log out</button></div>`;
+//LOGGED IN
+const loggedInHeadTemp = `<button id="logOutBtn">Log out</button></div>`;
 const loggedInMainTemp = `<div><h1>Welcome</h1></div>`;
 
 
 //SET VIEW BASED ON CURRENTUSER IN LS TRUE/FALSE
-(!localStorage.getItem("currentUser")) ? loggedOut() : loggedIn();
+if(!localStorage.getItem("currentUser")) {
+   loggedOut() 
+} else {
+    let user = JSON.parse(localStorage.getItem('currentUser'));
+    console.log(user)
+    loggedIn(user.username);
+} 
 
 
-//VUE FUNCTIONS
-function loggedIn() {
+//////VUE FUNCTIONS
+
+//VUE
+function vue(head, main) {
     headerAside.innerHTML = ""
-    headerAside.insertAdjacentHTML("beforeend", loggedInHeadTemp);
+    root.innerHTML = "";
+    headerAside.insertAdjacentHTML("beforeend", head);
+    root.insertAdjacentHTML("beforeend", main);
+};
+
+
+// LOGGED IN
+function loggedIn(username) {
+    headerAside.innerHTML = ""
+    headerAside.insertAdjacentHTML("beforeend", `<div>${username} ` + loggedInHeadTemp);
     
     root.innerHTML = "";
     root.insertAdjacentHTML("beforeend", loggedInMainTemp);
@@ -47,12 +77,9 @@ function loggedIn() {
 
 };
 
+//LOGGED OUT
 function loggedOut() {
-    headerAside.innerHTML = ""
-    headerAside.insertAdjacentHTML("beforeend", loggedOutHeadTemp);
-    
-    root.innerHTML = "";
-    root.insertAdjacentHTML("beforeend", loggedOutMainTemp);
+    vue(loggedOutHeadTemp, loggedOutMainTemp);
 
     document.querySelector("#regBtn").addEventListener("click", () => register());
     
@@ -60,8 +87,11 @@ function loggedOut() {
 
         let userName = document.querySelector("#username").value;
         let passWord = document.querySelector("#password").value;
-        //if(userName != " " || passWord != " "){
+       
+        if(userName != "" || passWord != ""){
+   
             let checkUser = {username: userName, password: passWord};
+            
             fetch("http://localhost:3050/users/login", {
                 method: "post", 
                 headers: {
@@ -71,26 +101,49 @@ function loggedOut() {
             })
             .then(res => res.json())
             .then(data => {
-                localStorage.setItem('currentUser', JSON.stringify(data.id))
-                loggedIn();
+                localStorage.setItem('currentUser', JSON.stringify(data))
+                loggedIn(data.username);
             });
             
             
-        // } else {
-        // register();
-        // };
+        } else {
+            document.querySelector("#username").style.border = "1px solid red";
+            document.querySelector("#password").style.border = "1px solid red";
+           
+        };
     
     });
 
 };
 
+//REGISTER
 function register() {
-    headerAside.innerHTML = ""
-    headerAside.insertAdjacentHTML("beforeend", regHeadTemp);
-    
-    root.innerHTML = "";
-    root.insertAdjacentHTML("beforeend", regMainTemp);
+   vue(regHeadTemp, regMainTemp);
 
+    document.querySelector("#createAccount").addEventListener("click", () => {
+        let regUser = {
+            username: document.querySelector("#regUN").value, 
+            firstname: document.querySelector("#firstname").value,
+            lastname: document.querySelector("#lastname").value,
+            email: document.querySelector("#email").value,
+            password: document.querySelector("#regPW").value,
+            subscribe: document.querySelector("#checkbox").checked,
+            id: ""
+        };
+
+        fetch("http://localhost:3050/users/createAccount", {
+            method: "post", 
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(regUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            localStorage.setItem('currentUser', JSON.stringify(data))
+            loggedIn(data.username);
+        });    
+    });
 };
 
 
