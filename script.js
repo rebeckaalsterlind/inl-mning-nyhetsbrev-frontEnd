ec797eef2118cfc19f6f5258e6188ab27cfe1e0f
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-04-25 14:15:52 +0200

    3

--- script.js
+++ script.js
@@ -161,7 +161,7 @@ function loggedIn(username) {
     document.querySelector("#userDetails").addEventListener("click", () => {
        
         let user = JSON.parse(localStorage.getItem('currentUser'));
-        console.log('user', user);
+        console.log('user', user.id);
         const userId = {
             id: user.id
         }
